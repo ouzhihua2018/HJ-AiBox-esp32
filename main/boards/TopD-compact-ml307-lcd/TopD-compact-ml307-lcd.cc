c14986947688d60fb375e45a2de129a6bc7c93c1
Michael Ou <michael.ou@live.com>
2025-06-05 15:40:46 +0800

    fix bugs

--- main/boards/TopD-compact-ml307-lcd/TopD-compact-ml307-lcd.cc
+++ main/boards/TopD-compact-ml307-lcd/TopD-compact-ml307-lcd.cc
@@ -22,16 +22,16 @@
 
 #define TAG "TOPD_COMPACT_ML307_LCD"
 
-LV_FONT_DECLARE(font_puhui_20_4);
-LV_FONT_DECLARE(font_awesome_20_4);
+LV_FONT_DECLARE(font_puhui_16_4);
+LV_FONT_DECLARE(font_awesome_16_4);
 
 class TOPD_COMPACT_ML307_LCD : public DualNetworkBoard {
 private:
  
     Button boot_button_;
+    Button volume_up_button_;
+    Button volume_down_button_;
     SpiLcdDisplay* display_;
-    esp_lcd_panel_io_handle_t panel_io_ = nullptr;
-    esp_lcd_panel_handle_t panel_ = nullptr;
 
     void InitializeSpi() {
         spi_bus_config_t buscfg = {};
@@ -45,6 +45,9 @@ private:
     }
 
     void InitializeSt7789Display() {
+         esp_lcd_panel_io_handle_t panel_io_ = nullptr;
+         esp_lcd_panel_handle_t panel_ = nullptr;
+        // 液晶屏控制IO初始化
         ESP_LOGD(TAG, "Install panel IO");
         esp_lcd_panel_io_spi_config_t io_config = {};
         io_config.cs_gpio_num = DISPLAY_CS;
@@ -71,8 +74,8 @@ private:
         display_ = new SpiLcdDisplay(panel_io_, panel_, DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_OFFSET_X, DISPLAY_OFFSET_Y, 
             DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y, DISPLAY_SWAP_XY, 
         {
-            .text_font = &font_puhui_20_4,
-            .icon_font = &font_awesome_20_4,
+            .text_font = &font_puhui_16_4,
+            .icon_font = &font_awesome_16_4,
 #if CONFIG_USE_WECHAT_MESSAGE_STYLE
             .emoji_font = font_emoji_32_init(),
 #else
@@ -93,6 +96,39 @@ private:
             }
             app.ToggleChatState();
         });
+            boot_button_.OnLongPress([this]() {
+            SwitchNetworkType();
+        });
+
+        volume_up_button_.OnClick([this]() {
+            auto codec = GetAudioCodec();
+            auto volume = codec->output_volume() + 10;
+            if (volume > 100) {
+                volume = 100;
+            }
+            codec->SetOutputVolume(volume);
+            GetDisplay()->ShowNotification(Lang::Strings::VOLUME + std::to_string(volume/10));
+        });
+
+        volume_up_button_.OnLongPress([this]() {
+            GetAudioCodec()->SetOutputVolume(100);
+            GetDisplay()->ShowNotification(Lang::Strings::MAX_VOLUME);
+        });
+
+        volume_down_button_.OnClick([this]() {
+            auto codec = GetAudioCodec();
+            auto volume = codec->output_volume() - 10;
+            if (volume < 0) {
+                volume = 0;
+            }
+            codec->SetOutputVolume(volume);
+            GetDisplay()->ShowNotification(Lang::Strings::VOLUME + std::to_string(volume/10));
+        });
+
+        volume_down_button_.OnLongPress([this]() {
+            GetAudioCodec()->SetOutputVolume(0);
+            GetDisplay()->ShowNotification(Lang::Strings::MUTED);
+        });
     }
 
     // 物联网初始化，添加对 AI 可见设备
@@ -110,7 +146,9 @@ private:
 public:
     TOPD_COMPACT_ML307_LCD() :
         DualNetworkBoard(ML307_TX_PIN, ML307_RX_PIN, 4096),
-        boot_button_(BOOT_BUTTON_GPIO) {
+        boot_button_(BOOT_BUTTON_GPIO),
+        volume_up_button_(VOLUME_UP_BUTTON_GPIO),
+        volume_down_button_(VOLUME_DOWN_BUTTON_GPIO)  {
         InitializeSpi();
         InitializeSt7789Display();
         InitializeButtons();
