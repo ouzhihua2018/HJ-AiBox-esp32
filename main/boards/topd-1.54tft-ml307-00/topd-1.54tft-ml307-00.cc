85b2388b780f8e4b62e65080d0f0e8cb71327be7
dollycode <a2435208397@163.com>
2025-09-02 10:59:45 +0800

    local voice modify

--- main/boards/topd-1.54tft-ml307-00/topd-1.54tft-ml307-00.cc
+++ main/boards/topd-1.54tft-ml307-00/topd-1.54tft-ml307-00.cc
@@ -215,7 +215,7 @@ public:
     }
     
     virtual Led* GetLed() override {
-        static CircularStrip led(BUILTIN_LED_GPIO, 2);
+        static CircularStrip led(BUILTIN_LED_GPIO, 30);
         return &led;
     }
     
